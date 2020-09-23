ef630915b0dcbe0613a0924c675ca587c2e6e837
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-09-23 12:53:43 +0200

    done

--- AspGateway/Controllers/AuthorsController.cs
+++ AspGateway/Controllers/AuthorsController.cs
@@ -13,50 +13,85 @@ namespace AspGateway.Controllers
     [Route("[controller]")]
     public class AuthorsController : ControllerBase
     {
-    
+
 
         [HttpGet]
-        public  string Get()
+        public string Get()
         {
-            HttpClient client = new HttpClient();    
-            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");     
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");
             HttpResponseMessage response = client.GetAsync("authors").Result;  // Blocking call!  
-            var authors =  response.Content.ReadAsStringAsync().Result;   
-            return  authors;
+            var authors = response.Content.ReadAsStringAsync().Result;
+            return authors;
 
         }
 
         [HttpGet("{id}")]
-        public  string Get(int id)
+        public string Get(int id)
         {
-            HttpClient client = new HttpClient();    
-            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");     
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");
             HttpResponseMessage response = client.GetAsync($"authors/{id}").Result;  // Blocking call!  
-            var authors =  response.Content.ReadAsStringAsync().Result;   
-            return  authors;
+            var authors = response.Content.ReadAsStringAsync().Result;
+            return authors;
 
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostAsync([FromForm] Author author)      
+        public async Task<ActionResult> Post([FromForm] Author author)
         {
-
-
             var client = new HttpClient();
 
             client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");
             var content = new FormUrlEncodedContent(new[]
             {
-                new KeyValuePair<string, string>("name", "sheriff"),
-                new KeyValuePair<string, string>("country", "egypt"),
-                new KeyValuePair<string, string>("gender", "male"),
+                new KeyValuePair<string, string>("name", author.Name),
+                new KeyValuePair<string, string>("country", author.Country),
+                new KeyValuePair<string, string>("gender", author.Gender),
             });
-
 
             var result = await client.PostAsync("authors", content);
             string resultContent = await result.Content.ReadAsStringAsync();
             return Ok(new { resultContent, content });
+        }
 
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put([FromForm] Author author, int id)
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("name", author.Name),
+                new KeyValuePair<string, string>("country", author.Country),
+                new KeyValuePair<string, string>("gender", author.Gender),
+            });
+
+
+            //content.Headers.Clear();
+            //content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+
+            var result = await client.PutAsync($"authors/{id}", content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            return Ok(new { resultContent, content });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost/LumenMicroservices/AuthorsApi/public/");
+
+            //content.Headers.Clear();
+            //content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+            var result = await client.DeleteAsync($"authors/{id}");
+            string resultContent = await result.Content.ReadAsStringAsync();
+            return Ok(new { resultContent });
         }
     }
 }
